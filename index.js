d4f43a9b4f56e528f79706471ed0011191a2280d
root <root@SURFACE.>
2024-04-03 16:06:12 +0300

    fetch

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name,email){
+const newdata =  {
+    name:name,
+    email:email
+}
+const configuredData ={
+    method:'POST',
+   headers:{
+   "COntent-Type": "application/json","Accept":"application/json"
+   },
+   body: JSON.stringify(newdata)
+};
+
+return fetch("http://localhost:3000/users",configuredData)
+    .then(res=>res.json())
+    .then(data=>{
+        document.body.innerHTML += `<p>User ID: ${data.id}</p>`;
+    })
+    .catch(function(error){
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+}
+submitData("beuce","beuce@hotmail.com")
